a9b3961bcdff69b25ac8565e933360e8a9726a44
Nahal Malik <nahalimran2001@gmail.com>
2025-08-04 17:22:17 -0700

    new

--- mentorcraft-backend/mentorcraft/mentorcraft/settings.py
+++ mentorcraft-backend/mentorcraft/mentorcraft/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -99,6 +100,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ✅ collectstatic target
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # ✅ for custom static files
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
